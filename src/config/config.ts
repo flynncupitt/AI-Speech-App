5d913624b57a8955dd6566803260585a45057462
Aquitaine24 <azmaand1n@gmail.com>
2024-08-21 16:59:50 +1200

    changed api key paramaters

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,10 +2,11 @@ require('dotenv').config();
 export const config = {
     firebaseConfig: {
         apiKey: process.env.API_KEY,
-        authDomain: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: ""
+        authDomain: "ai-speech-app-82a66.firebaseapp.com",
+        projectId: "ai-speech-app-82a66",
+        storageBucket: "ai-speech-app-82a66.appspot.com",
+        messagingSenderId: "171167486699",
+        appId: "1:171167486699:web:95be880595bc72131e24ae",
+        measurementId: "G-FKN0YN8V11"
     }
 };
