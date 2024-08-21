a67a8a9818135809e608d8328bfafe9872ba2a82
Azmaan Din <azmaand1n@gmail.com>
2024-08-21 20:12:54 +1200

    api update

--- src/config/firebaseconfig.ts
+++ src/config/firebaseconfig.ts
@@ -1,6 +1,11 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
 require('dotenv').config();
-export const config = {
-    firebaseConfig: {
+
+
+const firebaseConfig = {
         apiKey: process.env.API_KEY,
         authDomain: "ai-speech-app-82a66.firebaseapp.com",
         projectId: "ai-speech-app-82a66",
@@ -8,5 +13,10 @@ export const config = {
         messagingSenderId: "171167486699",
         appId: "1:171167486699:web:95be880595bc72131e24ae",
         measurementId: "G-FKN0YN8V11"
-    }
+
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const storage = getStorage(app);
