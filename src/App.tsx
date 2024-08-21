e41f2e50f6ca786361a2ada4ef7084310d33ad01
Azmaan Din <azmaand1n@gmail.com>
2024-08-21 20:29:17 +1200

    Changed the way we connect to firebase api

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { initializeApp } from "firebase/app";
-import { config } from "./config/config";
-
-//initializeApp(config.firebaseConfig);
 
 function App() {
   const [count, setCount] = useState(0);
