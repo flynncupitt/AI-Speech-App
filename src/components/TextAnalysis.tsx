65177289cfcf3d712aa80ca63493df695e9c312a
flynncupitt <132234350+flynncupitt@users.noreply.github.com>
2024-09-19 20:10:43 +1200

    small changes

---
+++ src/components/TextAnalysis.tsx
@@ -0,0 +1,44 @@
+
+
+//EXAMPLE WORK THAT COULD BE IMPLEMENTED IF NEEDED
+
+
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const TextAnalysis = () => {
+  const [text, setText] = useState("");
+  const fillerWords = ["um", "ah", "so", "uh"];
+  const [highlightedText, setHText] = useState("");
+
+  const analyseText = () => {
+    const arr = text.split(" ");
+    console.log(arr);
+
+    var outputText = arr
+      .map((word) => {
+        if (fillerWords.includes(word.toLowerCase())) {
+          return `<mark>${word}</mark>`;
+        }
+        return word;
+      })
+      .join(" ");
+    setHText(outputText);
+  };
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4 w-[80vw]">
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="textarea textarea-secondary w-6/12 h-20"
+        placeholder="Enter text here"
+      ></textarea>
+      <button className="btn bg-primary" onClick={() => analyseText()}>
+        Analyse Text
+      </button>
+      <p dangerouslySetInnerHTML={{ __html: highlightedText }}></p>
+    </div>
+  );
+};
+
+export default TextAnalysis;
