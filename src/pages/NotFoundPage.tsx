43129db987a65287be8c4f47b2f7f9afacab6229
DanVann123 <thnavann@gmail.com>
2024-09-19 20:23:52 +1200

    Updated 404page
    removed comments

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Assuming you are using React Router for navigation
+import { Link } from "react-router-dom";
 
 const NotFoundPage: React.FC = () => {
   return (
