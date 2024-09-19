8dca71c1c32da8e4c905a740f9705c41b54da49a
flynncupitt <132234350+flynncupitt@users.noreply.github.com>
2024-09-19 15:49:21 +1200

    cleaning

--- src/pages/UserDashboard.tsx
+++ src/pages/UserDashboard.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import DashboardHeader from "../components/LayoutHeader";
-import SpeechTextTest from "../components/SpeechTextTest";
+// import SpeechTextTest from "../components/SpeechTextTest";
 
 export const UserDashboard = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center">
       <p>The dashboard</p>
       {/* <SpeechTextTest></SpeechTextTest> */}
     </div>
