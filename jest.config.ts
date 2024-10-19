2935d3bf985ffe3eef4cd2e2b2465d12b6c82d01
Azmaan Din <azmaand1n@gmail.com>
2024-10-20 03:48:49 +1300

    configuring tests

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,7 @@ export default {
     "^.+\\.svg$": "jest-transformer-svg",
     "^@/(.*)$": "<rootDir>/src/$1",
     "^js-confetti$": "<rootDir>/node_modules/js-confetti/dist/js-confetti.browser.js",
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
   },
 
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
